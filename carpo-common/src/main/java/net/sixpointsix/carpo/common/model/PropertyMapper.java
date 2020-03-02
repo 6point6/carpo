29c6d00b95f37ed2fcd77d10d57497c4404f2a5c
aaronwitter <aaronwitter@hotmail.com>
2020-03-02 11:39:35 +0000

    set up for implementation and testing

---
+++ carpo-common/src/main/java/net/sixpointsix/carpo/common/model/PropertyMapper.java
@@ -0,0 +1,10 @@
+package net.sixpointsix.carpo.common.model;
+
+import java.util.Optional;
+
+public class PropertyMapper {
+
+    Optional<String> getStringValue(PropertyHoldingEntity entity, String path) {
+        return null;
+    }
+}
---
+++ carpo-common/src/test/java/net/sixpointsix/carpo/common/model/PropertyMapperTest.java
@@ -0,0 +1,82 @@
+package net.sixpointsix.carpo.common.model;
+
+import net.sixpointsix.carpo.common.model.immutable.AbstractImmutableCarpoPropertyEntity;
+import net.sixpointsix.carpo.common.model.immutable.ImmutableProperty;
+import net.sixpointsix.carpo.common.model.immutable.ImmutableTimestamp;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PropertyMapperTest {
+
+    public static class ExampleNestedObject {
+        private Object a;
+
+        public ExampleNestedObject(Object a) {
+            this.a = a;
+        }
+
+        public Object getA() {
+            return a;
+        }
+    }
+
+    public static class ExampleStringObject {
+        private String a;
+
+        public ExampleStringObject(String a) {
+            this.a = a;
+        }
+
+        public String getA() {
+            return a;
+        }
+    }
+
+    private static class ExampleEntity extends AbstractImmutableCarpoPropertyEntity {
+
+        public ExampleEntity(String carpoId, Timestamp timestamp, PropertyCollection properties) {
+            super(carpoId, timestamp, properties);
+        }
+    }
+
+    private static class ExampleBuilder extends AbstractPropertyEntityBuilder<PropertyMapperTest.ExampleEntity> {
+
+        @Override
+        public PropertyMapperTest.ExampleEntity build() {
+            return new ExampleEntity(carpoId, timestamp, properties);
+        }
+    }
+
+    @Test
+    void shouldReturnNestedValue() {
+        String id = UUID.randomUUID().toString();
+        Timestamp timestamp = ImmutableTimestamp.build();
+
+        ExampleStringObject eso = new ExampleStringObject("d");
+        Property property = ImmutableProperty.build("c", eso);
+
+        ExampleNestedObject eno = new ExampleNestedObject(property);
+        Property propertyTwo = ImmutableProperty.build("b", eno);
+
+        PropertyMapperTest.ExampleBuilder builder = new ExampleBuilder();
+        builder.setCarpoId(id);
+        builder.setTimestamp(timestamp);
+        builder.addProperty(ImmutableProperty.build("a", propertyTwo));
+
+        PropertyMapperTest.ExampleEntity exampleEntity = builder.build();
+        exampleEntity.getProperties();
+
+        PropertyMapper mapper = new PropertyMapper();
+
+        assertEquals(mapper.getStringValue(exampleEntity, "a.b.c").get(), "d");
+        assertEquals(mapper.getStringValue(exampleEntity, "a.b").get(), "c");
+        assertEquals(mapper.getStringValue(exampleEntity, "a").get(), "b");
+        assertEquals(mapper.getStringValue(exampleEntity, "a.b.c.d.e").get(), "");
+
+
+    }
+
+}
