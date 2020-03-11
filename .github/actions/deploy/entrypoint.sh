b5160e7ff67b688cd826fcb6d845e6c3e7c7cf50
Andrew Tarry <andrew.tarry@6point6.co.uk>
2020-03-11 15:40:19 +0000

    add release module

---
+++ .github/actions/deploy/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+module=${1}
+gpg_passphrase=${4}
+gpg_private_key=${5}
+export MAVEN_NEXUS_USER=${2}
+export MAVEN_NEXUS_PASSWORD=${3}
+
+echo "${gpg_private_key}" > private-key.txt && gpg --import --no-tty --batch --yes private-key.txt
+cd "${module}" || exit
+mvn deploy -Dgpg.passphrase="${gpg_passphrase}" -DskipTests -P deploy
