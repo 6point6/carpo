2ad59a338d24960113cebb7328e8893d3a71ed63
Andrew Tarry <andrew.tarry@6point6.co.uk>
2020-03-13 09:39:38 +0000

    add docs

--- carpo-common/src/main/java/net/sixpointsix/carpo/common/validation/RequiredProperties.java
+++ carpo-common/src/main/java/net/sixpointsix/carpo/common/validation/RequiredProperties.java
@@ -4,6 +4,27 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
+/**
+ * Validation for property holding entities
+ *
+ * <p>
+ *     Any property holding entity might need to have a limitation on the properties it will accept
+ * </p>
+ *
+ * <code>
+ *         @RequiredProperties(
+ *             required = {
+ *                     @RequiredProperty(key = "a", propertyType = PropertyType.STRING),
+ *                     @RequiredProperty(key = "c", propertyType = PropertyType.STRING)
+ *             },
+ *             allowUnknown = false
+ *     )
+ *     private static class KnownProperty implements PropertyHoldingEntity
+ * </code>
+ *
+ * @author Andrew Tarry
+ * @since 0.6.0
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = RequiredPropertyValidator.class)
@@ -16,7 +37,24 @@ public @interface RequiredProperties {
 
     Class<? extends Payload>[] payload() default {};
 
+    /**
+     * Set of annotations for each property
+     *
+     * <code>
+     *         @RequiredProperties(
+     *             required = {
+     *                     @RequiredProperty(key = "a", propertyType = PropertyType.STRING),
+     *                     @RequiredProperty(key = "c", propertyType = PropertyType.STRING)
+     *             }
+     *     )
+     * </code>
+     * @return requirements
+     */
     RequiredProperty[] required() default {};
 
+    /**
+     * Set to false if properties that are not in the required block should cause a violation
+     * @return false to trigger violations
+     */
     boolean allowUnknown() default true;
 }
